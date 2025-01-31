41a91549a9c7b2c3c6ed2b5e5cfe7c776e24700b
Jitesh Kumar <105545377+jitesh-20@users.noreply.github.com>
2025-01-31 19:10:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+function moveNext(input) {
+            if (input.value.length === 1) {
+                let next = input.nextElementSibling;
+                if (next) next.focus();
+            }
+        }
+        
+        function handleBackspace(input) {
+            if (event.key === "Backspace" && input.value.length === 0) {
+                let prev = input.previousElementSibling;
+                if (prev) prev.focus();
+            }
+        }
